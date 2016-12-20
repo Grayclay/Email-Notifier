82f032a910c0c6cc71d71bda2d2fc4f2c3c2df68
Klay <grayscripts@gmail.com>
2016-12-19 20:01:40 -0800

    new method to parse URLs, much more effecient and specific

--- Drafting/parsing_urls.py
+++ Drafting/parsing_urls.py
@@ -13,16 +13,24 @@ import smtplib
 # Import regular expressions
 import re 
 
+import urllib2
+
+import sys
+
+from email.MIMEMultipart import MIMEMultipart
+
+from email.MIMEText import MIMEText
+
+#-----------------------------------------------------------
+
+#scrape the page
 url = "http://sf.eater.com/the-shutter"
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 response = requests.get(url, headers=headers)
 
+#parse the HTML
 soup = BeautifulSoup(response.text, "html.parser")
 
-dates = []
-
-for item in soup.find_all(attrs={'class': 'p-byline__time'}):
-	print item.string
-	dates.append(item.string)
-	
-print dates
+for item in soup.find_all(attrs={'class': "m-entry-box__thumb "}):
+	for link in item.find_all("a"):
+		print link.get("href")
